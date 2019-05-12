9a635c1c53d3a04ad0bd966d38c8f21ab7e464a1
Samir Fragozo <samirfragozob@gmail.com>
2019-05-12 01:16:49 -0500

    auth layout

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.base')
+
+@section('title', __('base.titles.' . $crud))
+
+@section('body')
+    <div class="m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-grid--tablet-and-mobile m-grid--hor-tablet-and-mobile m-login m-login--1 m-login--signin">
+        <div class="m-grid__item m-grid__item--order-tablet-and-mobile-2 m-login__aside">
+            <div class="m-stack m-stack--hor m-stack--desktop">
+                <div class="m-stack__item m-stack__item--fluid">
+                    <div class="m-login__wrapper">
+                        <div class="m-login__logo">
+                            <a href="{{ route('home') }}">
+                                <img src="{{ asset('img/logo.png') }}" alt="{{ config('app.name') }}">
+                            </a>
+                        </div>
+                        <div class="m-login__signin">
+                            <div class="m-login__head">
+                                <h3 class="m-login__title">{{ __('base.titles.' . $crud) }}</h3>
+                                <div class="m-login__desc">{{ __('base.subtitles.' . $crud) }}</div>
+                            </div>
+                            @yield('content')
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="m-grid__item m-grid__item--fluid m-grid m-grid--center m-grid--hor m-grid__item--order-tablet-and-mobile-1 m-login__content m-grid-item--center"
+             style="background-image: url({{ asset('img/bg-auth.png') }})">
+        </div>
+    </div>
+@endsection
