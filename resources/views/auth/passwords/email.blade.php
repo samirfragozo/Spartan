46089ad499a3b7b93f758eec864bf434f85e327e
Samir Fragozo <samirfragozob@gmail.com>
2019-05-12 02:14:11 -0500

    email view

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,32 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+
+@php($crud = 'email')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
         </div>
-    </div>
-</div>
+    @endif
+
+    <form class="m-login__form m-form" action="{{ route('password.email') }}" method="POST">
+        @csrf
+
+        <div class="form-group m-form__group">
+            <label for="email"></label>
+            <input id="email" name="email" type="email" class="form-control m-input @error('email') is-invalid @enderror" value="{{ old('email') }}" placeholder="{{ __('base.attributes.email') }}" autocomplete="email" required autofocus>
+
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+
+        <div class="m-login__form-action">
+            <button class="btn btn-primary m-btn m-btn--pill m-btn--custom m-btn--air" type="submit">{{__('base.buttons.submit')}}</button>&nbsp;&nbsp;
+
+            <a href="{{ route('home') }}" class="btn btn-outline-primary m-btn m-btn--pill m-btn--custom">{{__('base.buttons.cancel')}}</a>
+        </div>
+    </form>
 @endsection
