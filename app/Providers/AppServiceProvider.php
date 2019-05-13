5cd94289e1845b907fa863a126e10eea9db0673f
Samir Fragozo <samirfragozob@gmail.com>
2019-05-13 07:55:51 -0500

    alpha_space validation rule

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,8 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Error correction SQLSTATE [42000] with MariaDB
-        Schema::defaultStringLength(191);
+        //
     }
 
     /**
@@ -25,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Error correction SQLSTATE [42000] with MariaDB
+        Schema::defaultStringLength(191);
+
+        // This rule only accept alphabet and spaces
+        Validator::extend('alpha_space', function ($attribute, $value) {
+            return preg_match('/^[\pL\s]+$/u', $value);
+        });
     }
 }
