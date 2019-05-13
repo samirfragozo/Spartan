4766fe515623e460121d41374b638fbf5fc1c164
Samir Fragozo <samirfragozob@gmail.com>
2019-05-13 08:01:47 -0500

    scripts include view

---
+++ resources/views/includes/scripts.blade.php
@@ -0,0 +1,13 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/caret/1.0.0/jquery.caret.min.js" integrity="sha256-NfP6KWI/oETcPbLcLXVAamn8K2wJrYH8ZIRrOf1XNUE=" crossorigin="anonymous" defer></script>
+<script type="text/javascript" src="https://cdn.datatables.net/plug-ins/1.10.18/api/page.jumpToData().js" defer></script>
+<script type="text/javascript" src="{{ asset('js/base/lang.js') }}" defer></script>
+<script type="text/javascript" src="{{ asset('js/base/tags.js') }}" defer></script>
+<script type="text/javascript" src="{{ asset('js/base/base.js') }}" defer></script>
+<script type="text/javascript" src="{{ asset('js/base/show.js') }}" defer></script>
+<script type="text/javascript" src="{{ asset('js/base/create.js') }}" defer></script>
+<script type="text/javascript" src="{{ asset('js/base/massive.js') }}" defer></script>
+<script type="text/javascript" src="{{ asset('js/base/filters.js') }}" defer></script>
+<script type="text/javascript" src="{{ asset('js/base/download.js') }}" defer></script>
+<script type="text/javascript" src="{{ asset('js/datatables/base.js') }}" defer></script>
+<script type="text/javascript" src="{{ asset('js/datatables/ajax.js') }}" defer></script>
+
