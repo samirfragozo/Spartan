02ade986ea2fea8ca8f5968171abddafcfaa59a5
Samir Fragozo <samirfragozob@gmail.com>
2019-05-15 09:26:48 -0500

    Punch

---
+++ database/factories/PunchFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Punch;
+use Faker\Generator as Faker;
+
+$factory->define(Punch::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
