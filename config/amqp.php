5ed5c4424d88d527ce4e7beaed636a454a49600c
Samir Fragozo <samirfragozob@gmail.com>
2019-05-14 22:02:58 -0500

    amqp config

---
+++ config/amqp.php
@@ -0,0 +1,63 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Define which configuration should be used
+    |--------------------------------------------------------------------------
+    */
+
+    'use' => 'production',
+
+    /*
+    |--------------------------------------------------------------------------
+    | AMQP properties separated by key
+    |--------------------------------------------------------------------------
+    */
+
+    'properties' => [
+
+        'production' => [
+            'host'                  => env('RABBIT_HOST', 'localhost'),
+            'port'                  => env('RABBIT_PORT', 5672),
+            'username'              => env('RABBIT_USERNAME', ''),
+            'password'              => env('RABBIT_PASSWORD', ''),
+            'vhost'                 => '/',
+            'connect_options'       => [],
+            'ssl_options'           => [],
+
+            'exchange'              => 'amq.direct',
+            'exchange_type'         => 'direct',
+            'exchange_passive'      => false,
+            'exchange_durable'      => true,
+            'exchange_auto_delete'  => false,
+            'exchange_internal'     => false,
+            'exchange_nowait'       => false,
+            'exchange_properties'   => [],
+
+            'queue_force_declare'   => false,
+            'queue_passive'         => false,
+            'queue_durable'         => true,
+            'queue_exclusive'       => false,
+            'queue_auto_delete'     => false,
+            'queue_nowait'          => false,
+            'queue_properties'      => ['x-ha-policy' => ['S', 'all']],
+
+            'consumer_tag'          => 'consumer',
+            'consumer_no_local'     => false,
+            'consumer_no_ack'       => false,
+            'consumer_exclusive'    => false,
+            'consumer_nowait'       => false,
+            'timeout'               => 0,
+            'persistent'            => false,
+
+            'qos'                   => false,
+            'qos_prefetch_size'     => 0,
+            'qos_prefetch_count'    => 1,
+            'qos_a_global'          => false
+        ],
+
+    ],
+
+];
