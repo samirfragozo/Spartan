2badd571482c29a210b86f2f5644d97dce542451
Samir Fragozo <samirfragozob@gmail.com>
2019-05-20 09:38:16 -0500

    listen rabbit

---
+++ app/Console/Commands/ListenRabbit.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Punch;
+use ErrorException;
+use Illuminate\Console\Command;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+class ListenRabbit extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:rabbit';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Catches Data from RabbitMQ';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     * @throws ErrorException
+     */
+    public function handle()
+    {
+        $connection = new AMQPStreamConnection('45.55.167.18', 5672, 'nox', '678Tppydk732dq4*');
+        $channel = $connection->channel();
+
+        $channel->queue_declare('Server', false, true, false, false);
+
+        echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+        $callback = function ($msg) {
+            $request = json_decode($msg->body);
+
+            $punch = new Punch;
+
+            $punch->date = $request->date;
+            $punch->client_id = $request->id;
+            $punch->access_id = $request->sucursal;
+
+            $punch->save();
+
+            echo ' [x] Received ', $msg->body, "\n";
+        };
+
+        $channel->basic_consume('Server', '', false, true, false, false, $callback);
+
+        while (count($channel->callbacks)) {
+            $channel->wait();
+        }
+    }
+}
