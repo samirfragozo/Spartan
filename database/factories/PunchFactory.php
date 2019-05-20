94856cb73b377bad8ebdb55f0f39db460ec1cc16
Samir Fragozo <samirfragozob@gmail.com>
2019-05-20 17:27:32 -0500

    punch factory

--- database/factories/PunchFactory.php
+++ database/factories/PunchFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factory;
 
 $factory->define(Punch::class, function (Faker $faker) {
     return [
-        'date' => $faker->dateTimeBetween($startDate = '-2 days', $endDate = 'now'),
+        'date' => $faker->dateTimeBetween($startDate = '-2 days', $endDate = '-1 day'),
         'client_id' => function () {
             return factory(App\Client::class)->create()->id;
         },
