993f94caa8512597ae9f488f79df70f865267c29
Samir Fragozo <samirfragozob@gmail.com>
2019-05-21 16:54:24 -0500

    amqp config

--- config/amqp.php
+++ config/amqp.php
@@ -2,62 +2,24 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Define which configuration should be used
-    |--------------------------------------------------------------------------
-    */
-
     'use' => 'production',
-
-    /*
-    |--------------------------------------------------------------------------
-    | AMQP properties separated by key
-    |--------------------------------------------------------------------------
-    */
-
     'properties' => [
-
         'production' => [
-            'host'                  => env('RABBIT_HOST', 'localhost'),
-            'port'                  => env('RABBIT_PORT', 5672),
-            'username'              => env('RABBIT_USERNAME', ''),
-            'password'              => env('RABBIT_PASSWORD', ''),
-            'vhost'                 => '/',
-            'connect_options'       => [],
-            'ssl_options'           => [],
-
-            'exchange'              => 'amq.direct',
-            'exchange_type'         => 'direct',
-            'exchange_passive'      => false,
-            'exchange_durable'      => true,
-            'exchange_auto_delete'  => false,
-            'exchange_internal'     => false,
-            'exchange_nowait'       => false,
-            'exchange_properties'   => [],
-
-            'queue_force_declare'   => false,
-            'queue_passive'         => false,
-            'queue_durable'         => true,
-            'queue_exclusive'       => false,
-            'queue_auto_delete'     => false,
-            'queue_nowait'          => false,
-            'queue_properties'      => ['x-ha-policy' => ['S', 'all']],
-
-            'consumer_tag'          => 'consumer',
-            'consumer_no_local'     => false,
-            'consumer_no_ack'       => false,
-            'consumer_exclusive'    => false,
-            'consumer_nowait'       => false,
-            'timeout'               => 0,
-            'persistent'            => false,
-
-            'qos'                   => false,
-            'qos_prefetch_size'     => 0,
-            'qos_prefetch_count'    => 1,
-            'qos_a_global'          => false
+            'host'                => env('RABBIT_HOST', 'localhost'),
+            'port'                => env('RABBIT_PORT', 5672),
+            'username'            => env('RABBIT_USERNAME', ''),
+            'password'            => env('RABBIT_PASSWORD', ''),
+            'vhost'               => '/',
+            'exchange'            => 'amq.direct',
+            'exchange_type'       => 'direct',
+            'exchange_durable'    => true,
+            'consumer_tag'        => 'consumer',
+            'ssl_options'         => [], // See https://secure.php.net/manual/en/context.ssl.php
+            'connect_options'     => [], // See https://github.com/php-amqplib/php-amqplib/blob/master/PhpAmqpLib/Connection/AMQPSSLConnection.php
+            'queue_properties'    => ['x-ha-policy' => ['S', 'all']],
+            'exchange_properties' => [],
+            'timeout'             => 0
         ],
-
     ],
 
 ];
