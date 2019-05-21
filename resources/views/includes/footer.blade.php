988094ccd158a1d91ae0a0e35fa25984519a7d8f
Samir Fragozo <samirfragozob@gmail.com>
2019-05-21 11:18:11 -0500

    footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="m-stack m-stack--flex-tablet-and-mobile m-stack--ver m-stack--desktop">
             <div class="m-stack__item m-stack__item--left m-stack__item--middle m-stack__item--last">
                 <span class="m-footer__copyright">
-                    2018 &copy; {{ config('app.name') }}
+                    2018 &copy; {{ config('app.name') }} powered by <a href="http://orioncol.com" class="m-link" rel="noreferrer" target="_blank">Orion Solutions</a>
                 </span>
             </div>
         </div>
