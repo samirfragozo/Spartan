cfd5db094b2db7c55fe4a727e04ad0f796a0a9cc
Samir Fragozo <samirfragozob@gmail.com>
2019-05-22 10:12:36 -0500

    test seeder

--- database/seeds/TestSeeder.php
+++ database/seeds/TestSeeder.php
@@ -14,13 +14,13 @@ class TestSeeder extends Seeder
      */
     public function run()
     {
-        for ($i = 0; $i < 50; $i++) {
+        for ($i = 0; $i < 10; $i++) {
             factory(Client::class)->create([
                 'active' => random_int(0, 1),
             ]);
         }
 
-        for ($i = 0; $i < 100; $i++) {
+        for ($i = 0; $i < 50; $i++) {
             factory(Punch::class)->create([
                 'client_id' => random_int(1, Client::count()),
             ]);
