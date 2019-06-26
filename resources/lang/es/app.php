4adb42a21118b6517d4766d4572e6392fa7f5529
Samir Fragozo <samirfragozob@gmail.com>
2019-06-26 15:02:45 -0500

    Remove person selects

--- resources/lang/es/app.php
+++ resources/lang/es/app.php
@@ -18,22 +18,6 @@ return [
         'contact_information' => 'Información de Contacto',
     ],
 
-    'selects' => [
-        'person' => [
-            'civil_status' => [
-                'C' => 'CASADO(A)',
-                'S' => 'SOLTERO(A)',
-                'U' => 'UNION LIBRE'
-            ],
-            'document_type' => [
-                'CC' => 'CÉDULA DE CIUDADANÍA',
-                'CE' => 'CÉDULA DE EXTRANJERÍA'
-            ],
-            'sex' => [
-                'F' => 'FEMENINO',
-                'M' => 'MASCULINO',
-            ],
-        ],
-    ],
+    'selects' => [],
 
 ];
