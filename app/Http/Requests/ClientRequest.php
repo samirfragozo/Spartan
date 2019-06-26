d9981d87668b012fa6ea90cfcde9904508463e58
Samir Fragozo <samirfragozob@gmail.com>
2019-06-26 15:34:23 -0500

    client request light

--- app/Http/Requests/ClientRequest.php
+++ app/Http/Requests/ClientRequest.php
@@ -15,17 +15,7 @@ class ClientRequest extends BaseRequest
     public function rules()
     {
         return [
-            'document_type' => 'required|in:' . implode(',', array_keys(__('app.selects.person.document_type'))),
-            'document' => 'required|numeric|digits_between:6,12|unique:clients,document,' . $this->id,
             'name' => 'required|min:3|max:50|alpha_space',
-            'last_name' => 'required|min:3|max:50|alpha_space',
-            'sex' => 'required|in:' . implode(',', array_keys(__('app.selects.person.sex'))),
-            'civil_status' => 'required|in:' . implode(',', array_keys(__('app.selects.person.civil_status'))),
-            'birth_date' => 'required|date|before:Today',
-            'address' => 'required|min:3|max:50',
-            'neighborhood' => 'required|min:3|max:50',
-            'phone' => 'required_without:cellphone|nullable|numeric|digits_between:6,12|bail',
-            'cellphone' => 'nullable|numeric|digits_between:6,12|bail',
             'rfid' => 'nullable|numeric|digits_between:4,12|unique:clients,rfid,' . $this->id . '|bail',
             'end' => 'required|date',
         ];
