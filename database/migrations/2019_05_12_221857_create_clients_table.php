623bdba0b881d55a600f8b286a700404ec860848
Samir Fragozo <samirfragozob@gmail.com>
2019-06-26 15:02:13 -0500

    migration light

--- database/migrations/2019_05_12_221857_create_clients_table.php
+++ database/migrations/2019_05_12_221857_create_clients_table.php
@@ -15,17 +15,7 @@ class CreateClientsTable extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->enum('document_type', array_keys(__('app.selects.person.document_type')));
-            $table->string('document', 12)->unique();
             $table->string('name', 50);
-            $table->string('last_name', 50);
-            $table->date('birth_date');
-            $table->enum('sex', array_keys(__('app.selects.person.sex')));
-            $table->enum('civil_status', array_keys(__('app.selects.person.civil_status')));
-            $table->string('address', 50);
-            $table->string('neighborhood', 50);
-            $table->string('phone', 15)->nullable();
-            $table->string('cellphone', 15)->nullable();
             $table->string('rfid', 12)->nullable();
             $table->date('end');
             $table->boolean('active')->default(1);
